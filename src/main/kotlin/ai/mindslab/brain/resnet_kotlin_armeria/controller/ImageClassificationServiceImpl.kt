a46743238658aae06ae497e10d518a97f0f352f1
BeLeap <beleap@beleap.codes>
2022-05-23 14:11:58 +0900

    collect streaming data

--- src/main/kotlin/ai/mindslab/brain/resnet_kotlin_armeria/controller/ImageClassificationServiceImpl.kt
+++ src/main/kotlin/ai/mindslab/brain/resnet_kotlin_armeria/controller/ImageClassificationServiceImpl.kt
@@ -3,12 +3,19 @@ package ai.mindslab.brain.resnet_kotlin_armeria.controller
 import ai.mindslab.brain.resnet_kotlin_armeria.ImageClassification
 import ai.mindslab.brain.resnet_kotlin_armeria.ImageClassificationServiceGrpcKt
 import ai.mindslab.brain.resnet_kotlin_armeria.classifyReply
+import ai.mindslab.brain.resnet_kotlin_armeria.service.ImageClassificationService
 import kotlinx.coroutines.flow.Flow
+import kotlinx.coroutines.flow.collect
+import kotlinx.coroutines.flow.onEach
 
 class ImageClassificationServiceImpl: ImageClassificationServiceGrpcKt.ImageClassificationServiceCoroutineImplBase() {
+    val service = ImageClassificationService()
     override suspend fun classify(requests: Flow<ImageClassification.ClassifyRequest>): ImageClassification.ClassifyReply {
+        var data = byteArrayOf()
+        requests.onEach { request -> data += request.data.toByteArray() }.collect()
+
         return classifyReply {
-            result = "Hi"
+            result = service.classify(data)
         }
     }
 }
---
+++ src/main/kotlin/ai/mindslab/brain/resnet_kotlin_armeria/service/ImageClassificationService.kt
@@ -0,0 +1,7 @@
+package ai.mindslab.brain.resnet_kotlin_armeria.service
+
+class ImageClassificationService {
+    fun classify(image: ByteArray): String {
+        return "Hi"
+    }
+}
